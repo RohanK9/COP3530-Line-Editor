40e4311850b31e61ef08e05a8010e13c4728c1fb
Rohan Kharwadkar <rohankharwadkar@ufl.edu>
2018-09-20 00:21:31 -0400

    nodes can be added

--- pa1.cpp
+++ pa1.cpp
@@ -17,11 +17,29 @@ public:
 		head = NULL;
 		tail = NULL;
 	}
+
+	void addToEnd(string text);
 };
+
+void list::addToEnd(string text){
+	node* temp = new node;
+	temp->data = text;
+	
+	
+
+	if (head == NULL){
+		head = temp;
+	}
+
+	
+}
 
 
 
 
 int main(){
+	list docx;
+	docx.addToEnd("pls");
+	
 	return 0;
 }
