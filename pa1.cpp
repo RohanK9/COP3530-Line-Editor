e6919f710de3025494bd7c0a56395cc9a5a94cff
Rohan Kharwadkar <rohankharwadkar@ufl.edu>
2018-09-20 15:29:57 -0400

    insertEnd functional

--- pa1.cpp
+++ pa1.cpp
@@ -11,36 +11,51 @@ class list{
 private:
 	node* head;
 	node* tail;
+	node* curr;
 
 public:
 	list(){
 		head = NULL;
 		tail = NULL;
+		curr = NULL;
 	}
 
 	void addToEnd(string text);
+	void PrintList();
 };
 
 void list::addToEnd(string text){
-	node* temp = new node;
-	temp->data = text;
-	
-	
+	node* toAdd = new node;
+	toAdd->next = NULL;
+	toAdd->data = text;
 
-	if (head == NULL){
-		head = temp;
+	if(head != NULL){
+		curr = head;
+		while(curr->next != NULL){
+			curr = curr->next;
+		}
+		curr->next = toAdd;
+	}
+	else{
+		head = toAdd;
 	}
 
-	
 }
 
-
-
+void list::PrintList(){
+	curr = head;
+	while(curr->next != NULL){
+		cout<<curr->data<<endl;
+		curr = curr->next;
+	}
+	cout<<curr->data<<endl;
+}
 
 int main(){
 	list docx;
-	docx.addToEnd("pls");
-	
+	docx.addToEnd("first node");
+	docx.addToEnd("second node");
+	docx.PrintList();
 	
 	return 0;
 }
