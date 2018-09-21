27c8371b04352afe8c304de1f69e4e63619e422f
Rohan Kharwadkar <rohankharwadkar@ufl.edu>
2018-09-21 00:13:21 -0400

    deleteLine completed

--- pa1.cpp
+++ pa1.cpp
@@ -23,6 +23,7 @@ public:
 	void addToEnd(string text);
 	void print();
 	void insert(string text, int position);
+	void deleteLine(int position);
 };
 
 //method to add to the end of the linked list
@@ -107,13 +108,30 @@ void list::insert(string text, int position){
 	}
 }
 
+void list::deleteLine(int position){
+	curr = head;
+	int count = 1;
+
+	if (position == 1){
+		head = head->next;
+	}
+
+	while(count < position - 1){
+		curr = curr->next;
+		count++;
+	}
+
+	curr->next = curr->next->next;
+}
+
 int main(){
 	list docx;
-	docx.addToEnd("now is the time");
-	docx.addToEnd("for all good men");
-	docx.addToEnd("to come to the aid of their country");
-	docx.insert("test1", 4);
-	docx.insert("test", 6);
+	docx.addToEnd("line one");
+	docx.addToEnd("line two");
+	docx.addToEnd("line three");
+	docx.insert("line four", 4);
+	docx.insert("line five", 5);
+	docx.deleteLine(5);
 	docx.print();
 	
 	return 0;
