2b1c3f87ef813988c29022805d9d65e6655c98a0
Rohan Kharwadkar <rohankharwadkar@ufl.edu>
2018-09-20 23:30:44 -0400

    insertEnd, insert, print done

--- pa1.cpp
+++ pa1.cpp
@@ -21,10 +21,13 @@ public:
 	}
 
 	void addToEnd(string text);
-	void PrintList();
+	void print();
+	void insert(string text, int position);
 };
 
+//method to add to the end of the linked list
 void list::addToEnd(string text){
+	//node to be added to the linked list
 	node* toAdd = new node;
 	toAdd->next = NULL;
 	toAdd->data = text;
@@ -36,26 +39,82 @@ void list::addToEnd(string text){
 		}
 		curr->next = toAdd;
 	}
+
+	//if the linked list is empty then the head becomes new node
 	else{
 		head = toAdd;
 	}
-
 }
 
-void list::PrintList(){
-	curr = head;
-	while(curr->next != NULL){
-		cout<<curr->data<<endl;
-		curr = curr->next;
+//method to print the linked list with line numbers
+void list::print(){
+
+	if (head == NULL){
+		cout << "Can't print an empty list" << endl;
+		return;
 	}
-	cout<<curr->data<<endl;
+	int lineCount = 1;
+	curr = head;
+
+	while(curr->next != NULL){
+		cout << lineCount << " " << curr->data << endl;
+		curr = curr->next;
+		lineCount++;
+	}
+
+	cout << lineCount << " " << curr->data << endl;
+}
+
+//method to insert a node into a given position in the linked list
+void list::insert(string text, int position){
+	node* toAdd = new node;
+	toAdd->next = NULL;
+	toAdd->data = text;
+
+	int lineCount = 1;
+	curr = head;
+
+	while(curr->next != NULL){
+		curr = curr->next;
+		lineCount++;
+	}
+
+	if(position > (lineCount + 1)){
+		cout << "The given position is out of bounds" << endl;
+		return;
+	} 
+
+	curr = head;
+	int count = 1;
+
+	if(position == 1){
+		toAdd->next = head;
+		head = toAdd;
+	}
+
+	else if(position > 1){
+		while(count < position - 1){
+			curr = curr->next;
+			count++;
+		}
+
+		toAdd->next = curr->next;
+		curr->next = toAdd;
+	}
+
+	else if(position == (lineCount + 1)){
+		addToEnd(text);
+	}
 }
 
 int main(){
 	list docx;
-	docx.addToEnd("first node");
-	docx.addToEnd("second node");
-	docx.PrintList();
+	docx.addToEnd("now is the time");
+	docx.addToEnd("for all good men");
+	docx.addToEnd("to come to the aid of their country");
+	docx.insert("test1", 4);
+	docx.insert("test", 6);
+	docx.print();
 	
 	return 0;
 }
